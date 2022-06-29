e23a3eb25112816f05e93e943ff9cf8d980f3d58
gaurav kumar <gaurav.kumar@netcomlearning.com>
2022-06-29 11:51:03 -0400

    added db docker image

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator function
+"""
+
+def add(x,y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x,y):
+    """Subtract x and y and return result"""
+    return x-y
